c97ca73b06988811edc9f51cecb69c6792788b21
Sughosh Rao <sughosh.rao@katalyst.com.au>
2022-10-06 12:21:08 +1030

    Add Trix editor support
    Also add styling

--- lib/katalyst/govuk/formbuilder.rb
+++ lib/katalyst/govuk/formbuilder.rb
@@ -1,4 +1,5 @@
 require "govuk_design_system_formbuilder"
+require "govuk_design_system_formbuilder/elements/rich_text_area"
 
 require "katalyst/govuk/formbuilder/engine"
 require "katalyst/govuk/formbuilder/frontend"
