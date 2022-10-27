27c10c8395fa94455797c99d9e170485f52a95df
Andy Williams <andy.williams@katalyst.com.au>
2022-10-27 20:32:32 +1030

    Add govuk_boolean_checkbox_field

--- lib/katalyst/govuk/formbuilder.rb
+++ lib/katalyst/govuk/formbuilder.rb
@@ -1,5 +1,6 @@
 require "govuk_design_system_formbuilder"
 require "govuk_design_system_formbuilder/elements/rich_text_area"
+require "govuk_design_system_formbuilder/elements/boolean_checkbox_field"
 
 require "katalyst/govuk/formbuilder/engine"
 require "katalyst/govuk/formbuilder/frontend"
