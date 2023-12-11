ee3d713857d2bb8a4d1f7e5df2e84e6f69d43573
Stephen Nelson <stephen@katalyst.com.au>
2023-12-11 10:36:29 +1030

    Default radio and checkbox size to "s"

--- lib/katalyst/govuk/formbuilder/extensions.rb
+++ lib/katalyst/govuk/formbuilder/extensions.rb
@@ -27,6 +27,86 @@ module GOVUKDesignSystemFormBuilder
     def fieldset_context
       @fieldset_context ||= []
     end
+
+    # Overwrite GOVUK default to set small to true
+    # @see GOVUKDesignSystemFormBuilder::Builder#govuk_collection_radio_buttons
+    def govuk_collection_radio_buttons(attribute_name, collection, value_method, text_method = nil, hint_method = nil,
+                                       hint: {}, legend: {}, caption: {}, inline: false, small: true, bold_labels: nil,
+                                       include_hidden: config.default_collection_radio_buttons_include_hidden,
+                                       form_group: {}, **kwargs, &block)
+      Elements::Radios::Collection.new(
+        self,
+        object_name,
+        attribute_name,
+        collection,
+        value_method:,
+        text_method:,
+        hint_method:,
+        hint:,
+        legend:,
+        caption:,
+        inline:,
+        small:,
+        bold_labels:,
+        form_group:,
+        include_hidden:,
+        **kwargs,
+        &block
+      ).html
+    end
+
+    # Overwrite GOVUK default to set small to true
+    # @see GOVUKDesignSystemFormBuilder::Builder#govuk_radio_buttons_fieldset
+    def govuk_radio_buttons_fieldset(attribute_name, hint: {}, legend: {}, caption: {}, inline: false, small: true,
+                                     form_group: {}, **kwargs, &block)
+      Containers::RadioButtonsFieldset.new(self, object_name, attribute_name,
+                                           hint:, legend:, caption:, inline:, small:, form_group:,
+                                           **kwargs, &block).html
+    end
+
+    # Overwrite GOVUK default to set small to true
+    # @see GOVUKDesignSystemFormBuilder::Builder#govuk_collection_check_boxes
+    def govuk_collection_check_boxes(attribute_name, collection, value_method, text_method, hint_method = nil,
+                                     hint: {}, legend: {}, caption: {}, small: true, form_group: {},
+                                     include_hidden: config.default_collection_check_boxes_include_hidden,
+                                     **kwargs, &block)
+      Elements::CheckBoxes::Collection.new(
+        self,
+        object_name,
+        attribute_name,
+        collection,
+        value_method:,
+        text_method:,
+        hint_method:,
+        hint:,
+        legend:,
+        caption:,
+        small:,
+        form_group:,
+        include_hidden:,
+        **kwargs,
+        &block
+      ).html
+    end
+
+    # Overwrite GOVUK default to set small to true
+    # @see GOVUKDesignSystemFormBuilder::Builder#govuk_check_boxes_fieldset
+    def govuk_check_boxes_fieldset(attribute_name, legend: {}, caption: {}, hint: {}, small: true, form_group: {},
+                                   multiple: true, **kwargs, &block)
+      Containers::CheckBoxesFieldset.new(
+        self,
+        object_name,
+        attribute_name,
+        hint:,
+        legend:,
+        caption:,
+        small:,
+        form_group:,
+        multiple:,
+        **kwargs,
+        &block
+      ).html
+    end
   end
 
   module Traits
